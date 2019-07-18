ad10e0f673fc74f600295c42a779a58ebdcf76a4
wojiaocc <707817839@qq.com>
2019-07-18 14:12:48 +0800

    -

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      git 钩子测试
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>修改内容</h3>
